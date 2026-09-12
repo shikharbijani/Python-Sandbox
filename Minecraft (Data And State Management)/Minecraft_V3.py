ec0b2b8ef2730454714c7ad093654c37d47f44b6
shikharbijani <shikharbijani@gmail.com>
2026-09-12 10:20:54 +0530

    Refactor entity classes and implement Skeleton
    Refactor entity classes and add Skeleton class for enhanced gameplay.

--- Minecraft (Data And State Management)/Minecraft_V3.py
+++ Minecraft (Data And State Management)/Minecraft_V3.py
@@ -1,13 +1,13 @@
 class Entity:
-    def __init__(self,name,hp=100):
+    def __init__(self,name: str,hp=100) -> None:
         self.name=name
         self.hp=hp
 
-    def take_damage(self,amount):
+    def take_damage(self,amount: int) -> None:
         self.hp-=amount
         print(f"{self.name} took {amount} damage, hp now is {self.hp}!")
 
-    def attack(self,target):
+    def attack(self,target) -> None:
         print(f"{self.name} hits {target.name}!")
         target.take_damage(5)
 
@@ -15,39 +15,54 @@ class Entity:
         return self.hp>0
 
 class Player(Entity):
-    def __init__(self,name,hp=100):
+    def __init__(self,name: str,hp=100) -> None:
         super().__init__(name,hp)
         self.damage_amount=20
         self.xp=0
 
-    def attack(self, target):
+    def attack(self, target: Entity) -> None:
         print(f"{self.name} hits {target.name}!")
         target.take_damage(self.damage_amount)
 
 class Mob(Entity):
-    def __init__(self,name,hp=100):
+    def __init__(self,name: str,hp=100) -> None:
         super().__init__(name,hp)
         self.xp_drop=20
 
-    def attack(self,target):
+    def attack(self,target: Entity) -> None:
             print(f"{self.name} hits {target.name}!")
             target.take_damage(5)
 
+class Skeleton(Mob):
+    def __init__(self,name: str,hp=100) -> None:
+        super().__init__(name,hp)
+        self.xp_drop=30
+
+    def attack(self, target: Entity) -> None:
+        print(f"{self.name} hits {target.name}!")
+        target.take_damage(10)
+
+def battle(player: Entity,mobs: list) -> None:
+    for mob in mobs:
+        while player.is_alive() and mob.is_alive():
+            player.attack(mob)
+            if not mob.is_alive():
+                print(f"{player.name} killes {mob.name}!")
+                player.xp+=mob.xp_drop
+                print(f"{player.name} now has {player.xp}!")
+                break
+            mob.attack(player)
+            if not player.is_alive():
+                print(f"{mob.name} killed {player.name}!")
+                return None
+    print(f"{player.name} won the battle!")
+    
 def main():
     steve=Player("Steve")
     zombie=Mob("Zombie")
-    skeleton=Mob("Skeleton")
-    while True:
-        steve.attack(skeleton)
-        if not skeleton.is_alive():
-            print(f"{steve.name} killed {skeleton.name}!")
-            steve.xp+=skeleton.xp_drop
-            print(f"{steve.name} now has {steve.xp} xp!")
-            break
-        skeleton.attack(steve)
-        if not steve.is_alive():
-            print(f"{skeleton.name} killed {steve.name}!")
-            break
+    skeleton=Skeleton("Skeleton")
+    mobs=[zombie,skeleton]
+    battle(steve,mobs)
         
 
 if __name__=="__main__":
