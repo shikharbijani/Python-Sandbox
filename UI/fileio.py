6813b9279ed810ee71a635a0a9d1dfbbb8cdce17
shikharbijani <shikharbijani@gmail.com>
2026-09-23 21:40:45 +0530

    Add files via upload

--- UI/fileio.py
+++ UI/fileio.py
@@ -1,5 +1,16 @@
 from tkinter import *
 from tkinter import filedialog
+def open_file():
+    filepath=filedialog.askopenfilename(initialdir="Documents",
+                                        filetypes=(("Text files","*.txt"),("all files","*.*")))
+
+    file=open(filepath,'r')
+    print(file.read())
+    file.close()
+    
 window=Tk()
 
-window.pack()
+button=Button(window,text="Open",command=open_file)
+button.pack()
+
+window.mainloop()
